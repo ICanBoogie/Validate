7b4ba16361f769d30cf58296b9002f17a31fd8e5
Olivier Laviale <olivier.laviale@gmail.com>
2017-04-08 13:44:20 +0200

    Fix Required

---
+++ tests/Validator/RequiredTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ICanBoogie\Validate\Validator;
+
+class RequiredTest extends ValidatorTestCase
+{
+	const VALIDATOR_CLASS = Required::class;
+
+	public function test_normalize_params()
+	{
+		$validator = new Required();
+
+		$this->assertSame([
+
+			Required::OPTION_STOP_ON_ERROR => true
+
+		], $validator->normalize_params([]));
+	}
+
+	/**
+	 * @return array
+	 */
+	public function provide_test_valid_values()
+	{
+		return [
+
+			[ true ],
+			[ false ],
+			[ '0' ],
+			[ [ 0 ] ],
+
+		];
+	}
+
+	/**
+	 * @return array
+	 */
+	public function provide_test_invalid_values()
+	{
+		return [
+
+			[ null ],
+			[ '' ],
+			[ ' ' ],
+			[ [] ],
+
+		];
+	}
+}
